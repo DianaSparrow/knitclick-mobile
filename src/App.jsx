8ca5e0ae0d436538326f8ceccc96c63e02ed7000
DianaSparrow <helloandreajanellc@gmail.com>
2025-06-07 17:10:30 -0400

    Set up KnitClick Mobile App Preview

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CounterScreen from './screens/CounterScreen';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <CounterScreen />
+    </div>
+  );
+};
+
+export default App;
